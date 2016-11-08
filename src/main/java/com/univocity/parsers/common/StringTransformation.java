96ac086de8336c1834337a79c343bc8afd3c9b8b
Jeronimo Backes <jbax@univocity.com>
2016-11-08 16:45:15 +1030

    - Enabling the use of transformations over content parsed from HTML fields. - Refactoring HTML parsing structure to allow downloads to be performed from any type of field.

---
+++ src/main/java/com/univocity/parsers/common/StringTransformation.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2013 uniVocity Software Pty Ltd. All rights reserved.
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE.txt', which is part of this source code package.
+ */
+
+package com.univocity.parsers.common;
+
+/**
+ * A transformation operation over a {@code String}, that results in another {@code String}
+ *
+ * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
+ */
+public interface StringTransformation extends Transformation<String, String> {
+
+}
